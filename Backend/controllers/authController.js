f6bcee64fdaeacdf12fb866d34d8c319a2c676f4
aadarshmunnanita27 <aadarshmunnacse@gmail.com>
2026-07-04 19:18:23 +0530

    implement user registration with password hashing

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,47 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Registration Successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -3,15 +3,20 @@ import "dotenv/config";
 import cors from "cors";
 import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
-
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+
+
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api", chatRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
