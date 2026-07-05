b667cc40f2739ccdaa0a45e52d18060d4a72462a
aadarshmunnanita27 <aadarshmunnacse@gmail.com>
2026-07-05 17:41:00 +0530

    commit

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -34,7 +34,9 @@ function Register() {
             navigate("/login");
 
         } catch (err) {
-
+             console.log(err);
+              console.log(err.response);
+              console.log(err.response?.data); 
             alert(err.response?.data?.message || "Registration Failed");
 
         }
